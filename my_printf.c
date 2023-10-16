5815dfb631ca2f13e6b1426f58fa6202676b76b5
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-16 18:54:44 +0300

    1st step

---
+++ my_printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _printf - this function produces the output 
+ * according to the format
+ * @format: is the format
+ * Return: the number of chars printed excluding
+ * NULL byte to end to output
+ */
+int _printf(const char *format, ...)
+{
+	int i;
+	int count = 0;
+	va_list mo;
+	if (format == NULL)
+		return (-1);
+	va_start(mo, format);
+
+	
+		for(i = 0; format[i] != '\0'; i++)
+		{
+			if(format[i] == '%' && format[i + 1] == 's')
+			{
+			count += print_string(va_arg(mo, char *));
+			i++;
+			}
+			else if (format[i] == '%' && format[i + 1] == 'c')
+			{
+			count += print_char(va_arg(mo, int));
+			i++;
+			}
+			else if (format[i] == '%' && format[i + 1] == '%')
+			{
+				_putchar('%');
+				count++;
+				i++;
+			}		
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
+		}
+		va_end(mo);
+
+		return (count);
+}
---
+++ print_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_char - prints a char
+ * @c: is the given char
+ * return: the number of char to be printed
+ */
+int print_char(char c)
+{
+	int i = 1;
+
+	_putchar(c);
+	return (i);
+}
