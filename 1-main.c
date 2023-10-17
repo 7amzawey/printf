e7ada702ac5616283c0aeb06644f1262b4cf6531
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-17 20:09:51 +0300

    cool

--- 1-main.c
+++ 1-main.c
@@ -5,10 +5,14 @@ int main(void)
 {
     char c = 'A';
     char *str = "OpenAI";
+    int i = 1;
+    int d = 5;
 
     _printf("Character: %c\n", c);
     _printf("String: %s\n", str);
     _printf("Percent sign: %%\n");
+    _printf("int: %d\n", d);
+    _printf("decimal: %i\n", i);
 
     return (0);
 }
--- my_printf.c
+++ my_printf.c
@@ -7,42 +7,46 @@
 int _printf(const char *format, ...)
 {
 	va_list list;
-	int i;
+	int j;
 	char c;
 	char *str;
 
 	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (j = 0; format && format[j] != '\0'; j++)
 	{
-		if (format[i] != '%')
+		if (format[j] != '%')
 		{
-			write(1, &format[i], 1);
+			write(1, &format[j], 1);
 		}
 		else
 		{
-			i++;
-			switch (format[i])
+			j++;
+			switch (format[j])
 			{
 			case 'c':
 			c = va_arg(list, int);
-
-				write(1, &c, 1);
-				break;
+			write(1, &c, 1);
+			break;
 			case 's':
 			str = va_arg(list, char *);
-
 			if (str)
-			{
-				write(1, str, strlen(str));
-			}
-
+			write(1, str, strlen(str));
 			break;
+			case 'd':
+			case 'i':
+			{
+			int num = va_arg(list, int);
+			char str[12];
+			sprintf(str, "%d", num);
+			write(1, str,strlen(str));
+			break;
+			}
 			case '%':
-			write(1, &format[i], 1);
+			write(1, &format[j], 1);
 			break;
 			}
 		}
 	}
 	va_end(list);
-	return (i);
+	return (j);
 }
